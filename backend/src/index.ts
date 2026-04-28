da428afb04309c50189ba1d15f94d978e53a7b8b
sanjanabharath <sanjanabarath@gmail.com>
2026-04-28 22:31:09 +0530

    Added firebase auth

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { initializeApp } from "firebase/app";
+import admin from "firebase-admin";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+admin.initializeApp(serviceAccount);
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
